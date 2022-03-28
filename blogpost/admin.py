7a71cea7ccf60e18f03a957fb257fd7547d47e27
Taro Noji <tailangyede9@gmail.com>
2022-03-28 10:30:40 +0900

    next commit

---
+++ blogpost/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import SampleModel, BlogModel    # --追記
+
+admin.site.register(SampleModel)   # --追記
+admin.site.register(BlogModel)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
